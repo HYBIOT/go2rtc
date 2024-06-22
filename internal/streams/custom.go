058da58fcaa331019be0944bfa955482ea38ed16
Kittisak Phormraksa <dnjooiopa@gmail.com>
2024-06-22 08:05:40 +0700

    Control RTSP speed

---
+++ internal/streams/custom.go
@@ -0,0 +1,58 @@
+package streams
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/AlexxIT/go2rtc/pkg/rtsp"
+)
+
+// only support RTSP sources
+func apiStreamsSpeed(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	streamName := query.Get("name")
+
+	if streamName == "" {
+		http.Error(w, "name required", http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method {
+	case "PUT":
+		streamsMu.RLock()
+		stream := Get(streamName)
+		defer streamsMu.RUnlock()
+
+		if stream == nil {
+			http.Error(w, "", http.StatusNotFound)
+			return
+		}
+
+		speedStr := query.Get("speed")
+		if speedStr == "" {
+			http.Error(w, "speed required", http.StatusBadRequest)
+			return
+		}
+
+		_, err := strconv.ParseFloat(speedStr, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		for _, producer := range stream.producers {
+			if conn, ok := producer.conn.(*rtsp.Conn); ok {
+				conn.Connection.Speed = speedStr
+				err := conn.Play()
+				if err != nil {
+					log.Error().Msgf("[stream] conn.Play(): %+v\n", err)
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+			}
+		}
+
+		return
+	}
+
+	http.Error(w, "", http.StatusNotFound)
+}
